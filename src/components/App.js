0e2bccb4b11f5771472f3c24d4571e556051737d
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-03-29 16:18:01 -0400

    redux

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../stylesheets/App.css';
+import Cart from "../containers/Cart"
+class App extends Component {
+  render() {
+    return (
+      <Cart />
+    );
+  }
+}
+
+export default App;
